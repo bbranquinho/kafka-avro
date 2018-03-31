1e00383a4e30b6c50d9ff87454db6a08f204316e
Emerson Borges <emersonbrg@gmail.com>
2018-03-31 14:42:55 -0300

    Refactor

--- consumer/src/main/kotlin/br/com/emersonborges/kafkaavro/Consumer.kt
+++ consumer/src/main/kotlin/br/com/emersonborges/kafkaavro/Consumer.kt
@@ -2,23 +2,27 @@ package br.com.emersonborges.kafkaavro
 
 import br.com.emersonborges.CustomerCreated
 import org.apache.avro.generic.GenericRecord
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Component
 
 @Component
 class Consumer {
 
+    val logger: Logger = LoggerFactory.getLogger(this.javaClass)
+
     @KafkaListener(topics = ["customer"], containerFactory = "specificKafkaListenerContainerFactory")
     fun specificConsumer(event: CustomerCreated) {
-        println("SPECIFIC RECORD")
-        println(event)
-        println(event.getFullName())
+        logger.info("SPECIFIC RECORD")
+        logger.info("Event: $event")
+        logger.info("Event customer fullName: ${event.getFullName()}")
     }
 
     @KafkaListener(topics = ["customer"], containerFactory = "genericKafkaListenerContainerFactory")
     fun genericConsumer(event: GenericRecord) {
-        println("GENERIC RECORD")
-        println(event)
-        println(event.get("fullName"))
+        logger.info("GENERIC RECORD")
+        logger.info("Event: $event")
+        logger.info("Event customer fullName: ${event.get("fullName")}")
     }
 }
--- producer/src/main/kotlin/br/com/emersonborges/kafkaavro/controller/EventsController.kt
+++ producer/src/main/kotlin/br/com/emersonborges/kafkaavro/controller/EventsController.kt
@@ -7,14 +7,17 @@ import org.apache.avro.generic.GenericRecord
 import org.apache.avro.io.DecoderFactory
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.header.internals.RecordHeader
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.core.io.Resource
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.io.ResourceLoader
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.kafka.core.KafkaTemplate
-import org.springframework.web.bind.annotation.*
-import java.io.File
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
 import java.io.IOException
+import java.io.InputStream
 
 
 @RestController
@@ -27,53 +30,41 @@ class EventsController(
         const val CUSTOMER_ADDRESS_ADDED_HEADER = "CustomerAddressAdded"
     }
 
-    @Value("classpath:avro/CustomerCreated_v1.avsc")
-    lateinit var schemaCustomerCreatedV1: Resource
-
-    @Value("classpath:avro/CustomerCreated_v2.avsc")
-    lateinit var schemaCustomerCreatedV2: Resource
-
-    @Value("classpath:avro/CustomerCreated_v3.avsc")
-    lateinit var schemaCustomerCreatedV3: Resource
-
-    @Value("classpath:avro/CustomerCreated_v4.avsc")
-    lateinit var schemaCustomerCreatedV4: Resource
-
-    @Value("classpath:avro/CustomerAddressAdded_v1.avsc")
-    lateinit var schemaCustomerAddressAddedV1: Resource
+    @Autowired
+    lateinit var resourceLoader: ResourceLoader
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(value = ["/v1/customers/events"], consumes = [(MediaType.APPLICATION_JSON_VALUE)])
     fun customerCreatedV1(@RequestBody event: String) {
-        kafkaTemplate.send(buildRecord(parseJson(schemaCustomerCreatedV1.file, event), CUSTOMER_CREATED_HEADER))
+        kafkaTemplate.send(buildRecord(parseJson(readSchema("CustomerCreated_v1"), event), CUSTOMER_CREATED_HEADER))
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(value = ["/v1/customers/addresses/events"], consumes = [(MediaType.APPLICATION_JSON_VALUE)])
     fun customerUpdatedV1(@RequestBody event: String) {
-        kafkaTemplate.send(buildRecord(parseJson(schemaCustomerAddressAddedV1.file, event), CUSTOMER_ADDRESS_ADDED_HEADER))
+        kafkaTemplate.send(buildRecord(parseJson(readSchema("CustomerAddressAdded_v1"), event), CUSTOMER_ADDRESS_ADDED_HEADER))
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(value = ["/v2/customers/events"], consumes = [(MediaType.APPLICATION_JSON_VALUE)])
     fun customerCreatedV2(@RequestBody event: String) {
-        kafkaTemplate.send(buildRecord(parseJson(schemaCustomerCreatedV2.file, event), CUSTOMER_CREATED_HEADER))
+        kafkaTemplate.send(buildRecord(parseJson(readSchema("CustomerCreated_v2"), event), CUSTOMER_CREATED_HEADER))
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(value = ["/v3/customers/events"], consumes = [(MediaType.APPLICATION_JSON_VALUE)])
     fun customerCreatedV3(@RequestBody event: String) {
-        kafkaTemplate.send(buildRecord(parseJson(schemaCustomerCreatedV3.file, event), CUSTOMER_CREATED_HEADER))
+        kafkaTemplate.send(buildRecord(parseJson(readSchema("CustomerCreated_v3"), event), CUSTOMER_CREATED_HEADER))
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(value = ["/v4/customers/events"], consumes = [(MediaType.APPLICATION_JSON_VALUE)])
     fun customerCreatedV4(@RequestBody event: String) {
-        kafkaTemplate.send(buildRecord(parseJson(schemaCustomerCreatedV4.file, event), CUSTOMER_CREATED_HEADER))
+        kafkaTemplate.send(buildRecord(parseJson(readSchema("CustomerCreated_v4"), event), CUSTOMER_CREATED_HEADER))
     }
 
     @Throws(IOException::class)
-    private fun parseJson(schema: File, json: String): GenericData.Record {
+    private fun parseJson(schema: InputStream, json: String): GenericData.Record {
         val parsedSchema = Schema.Parser().parse(schema)
         val decoder = DecoderFactory().jsonDecoder(parsedSchema, json)
         val reader = GenericDatumReader<GenericData.Record>(parsedSchema)
@@ -91,5 +82,9 @@ class EventsController(
             value,
             mutableListOf(RecordHeader("type", header.toByteArray()))
         )
+    }
+
+    private fun readSchema(schemaName: String) : InputStream {
+        return resourceLoader.getResource("classpath:avro/$schemaName.avsc").inputStream
     }
 }
