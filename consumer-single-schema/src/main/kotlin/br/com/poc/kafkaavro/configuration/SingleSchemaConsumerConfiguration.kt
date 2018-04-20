1bc9ab3ab9c1eb82f24287cc1c543bd7827b40fe
Emerson Borges <emersonbrg@gmail.com>
2018-04-20 01:00:34 -0300

    Packages refactor

---
+++ consumer-single-schema/src/main/kotlin/br/com/poc/kafkaavro/configuration/SingleSchemaConsumerConfiguration.kt
@@ -0,0 +1,70 @@
+package br.com.poc.kafkaavro.configuration
+
+import br.com.poc.CustomerEvent
+import io.confluent.kafka.serializers.KafkaAvroDeserializer
+import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig
+import io.confluent.kafka.serializers.KafkaAvroSerializerConfig
+import org.apache.avro.generic.GenericRecord
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.annotation.EnableKafka
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.core.ConsumerFactory
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+
+@Configuration
+@EnableKafka
+class SingleSchemaConsumerConfiguration {
+
+    @Value("\${spring.kafka.bootstrap-servers}")
+    private lateinit var bootstrapAddress: String
+
+    @Value("\${spring.kafka.group.id}")
+    private lateinit var groupId: String
+
+    @Value("\${schema.registry.url}")
+    private lateinit var schemaRegistryUrl: String
+
+    companion object {
+        const val CUSTOMER_CREATED_EVENT_TYPE = "CustomerCreated"
+        const val EVENT_TYPE_HEADER = "type"
+    }
+
+    @Bean
+    fun  genericConsumerFactory(): ConsumerFactory<String, GenericRecord> {
+        return buildConsumerFactory<GenericRecord>(group = "group2", isSpecificRecord = false)
+    }
+
+    @Bean
+    fun  specificConsumerFactory(): ConsumerFactory<String, CustomerEvent> {
+        return buildConsumerFactory<CustomerEvent>(group = "group3")
+    }
+
+    @Bean
+    fun specificKafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, CustomerEvent> {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, CustomerEvent>()
+        factory.consumerFactory = specificConsumerFactory()
+        return factory
+    }
+
+    @Bean
+    fun genericKafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, GenericRecord> {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, GenericRecord>()
+        factory.consumerFactory = genericConsumerFactory()
+        return factory
+    }
+
+    private fun <T> buildConsumerFactory(group: String = groupId, isSpecificRecord: Boolean = true): DefaultKafkaConsumerFactory<String, T> {
+        val configProps = HashMap<String, Any>()
+        configProps[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = bootstrapAddress
+        configProps[ConsumerConfig.GROUP_ID_CONFIG] = group
+        configProps[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
+        configProps[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = KafkaAvroDeserializer::class.java
+        configProps[KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG] = schemaRegistryUrl
+        configProps[KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG] = isSpecificRecord.toString()
+        return DefaultKafkaConsumerFactory(configProps)
+    }
+}
---
+++ producer-multiple-schemas/src/main/kotlin/br/com/poc/kafkaavro/configuration/KafkaProducerConfiguration.kt
@@ -0,0 +1,42 @@
+package br.com.poc.kafkaavro.configuration
+
+import io.confluent.kafka.serializers.KafkaAvroSerializer
+import io.confluent.kafka.serializers.KafkaAvroSerializerConfig
+import io.confluent.kafka.serializers.subject.RecordNameStrategy
+import org.apache.avro.generic.GenericRecord
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG
+import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.core.ProducerFactory
+import java.util.*
+
+@Configuration
+class KafkaProducerConfiguration {
+
+    @Value("\${spring.kafka.bootstrap-servers}")
+    private lateinit var bootstrapAddress: String
+
+    @Value("\${schema.registry.url}")
+    private lateinit var schemaRegistryUrl: String
+
+    @Bean
+    fun producerFactory(): ProducerFactory<String, GenericRecord> {
+        val configProps = HashMap<String, Any>()
+        configProps[BOOTSTRAP_SERVERS_CONFIG] = bootstrapAddress
+        configProps[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+        configProps[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = KafkaAvroSerializer::class.java
+        configProps[KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG] = schemaRegistryUrl
+        configProps[KafkaAvroSerializerConfig.VALUE_SUBJECT_NAME_STRATEGY] = RecordNameStrategy::class.java
+        return DefaultKafkaProducerFactory<String, GenericRecord>(configProps)
+    }
+
+    @Bean()
+    fun kafkaTemplate(): KafkaTemplate<String, GenericRecord> {
+        return KafkaTemplate(producerFactory())
+    }
+}
