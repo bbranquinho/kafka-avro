8101ff89a2c42c58d8add7175da2125ecd136428
Emerson Borges <emersonbrg@gmail.com>
2018-04-21 15:42:57 -0300

    Simplifying specific record consumer

--- consumer-multiple-schemas/src/main/kotlin/br/com/poc/kafkaavro/MultipleSchemasConsumer.kt
+++ consumer-multiple-schemas/src/main/kotlin/br/com/poc/kafkaavro/MultipleSchemasConsumer.kt
@@ -3,6 +3,7 @@ package br.com.poc.kafkaavro
 import br.com.poc.CustomerAddressAdded
 import br.com.poc.CustomerCreated
 import org.apache.avro.generic.GenericRecord
+import org.apache.avro.generic.IndexedRecord
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
@@ -13,24 +14,36 @@ class MultipleSchemasConsumer {
 
     val logger: Logger = LoggerFactory.getLogger(this.javaClass)
 
-    @KafkaListener(topics = ["customer"], containerFactory = "customerCreatedKafkaListenerContainerFactory")
-    fun customerCreatedConsumer(event: CustomerCreated) {
-        logger.info("CUSTOMER CREATED RECORD")
+    @KafkaListener(topics = ["customer"], containerFactory = "specificRecordKafkaListenerContainerFactory")
+    fun specificRecordConsumer(event: IndexedRecord) {
+        logger.info("SPECIFIC RECORD RECORD")
         logger.info("Event: $event")
-        logger.info("Event customer fullName: ${event.getFullName()}")
-    }
-
-    @KafkaListener(topics = ["customer"], containerFactory = "customerAddressAddedKafkaListenerContainerFactory")
-    fun customerAddressAddedConsumer(event: CustomerAddressAdded) {
-        logger.info("CUSTOMER ADDRESS ADDED RECORD")
-        logger.info("Event: $event")
-        logger.info("Event customer address street: ${event.getStreet()}")
+        when (event) {
+            is CustomerCreated -> {
+                logger.info("CUSTOMER CREATED RECORD")
+                logger.info("Event: $event")
+                logger.info("Event customer fullName: ${event.getFullName()}")
+            }
+            is CustomerAddressAdded -> {
+                logger.info("CUSTOMER ADDRESS ADDED RECORD")
+                logger.info("Event: $event")
+                logger.info("Event customer address street: ${event.getStreet()}")
+            }
+        }
     }
 
     @KafkaListener(topics = ["customer"], containerFactory = "genericKafkaListenerContainerFactory")
     fun genericConsumer(event: GenericRecord) {
         logger.info("GENERIC RECORD")
         logger.info("Event: $event")
-        logger.info("Event customer fullName: ${event.get("fullName")}")
+        if (event.schema.fullName == CustomerCreated.getClassSchema().fullName) {
+            logger.info("GENERIC CUSTOMER CREATED RECORD")
+            logger.info("Event customer fullName: ${event.get("fullName")}")
+        }
+        if (event.schema.fullName == CustomerAddressAdded.getClassSchema().fullName) {
+            logger.info("GENERIC CUSTOMER ADDRESS ADDED RECORD")
+            logger.info("Event customer address street: ${event.get("street")}")
+        }
     }
+
 }
--- consumer-multiple-schemas/src/main/kotlin/br/com/poc/kafkaavro/configuration/MultipleSchemasConsumerConfiguration.kt
+++ consumer-multiple-schemas/src/main/kotlin/br/com/poc/kafkaavro/configuration/MultipleSchemasConsumerConfiguration.kt
@@ -1,13 +1,11 @@
 package br.com.poc.kafkaavro.configuration
 
-import br.com.poc.CustomerAddressAdded
-import br.com.poc.CustomerCreated
 import io.confluent.kafka.serializers.KafkaAvroDeserializer
 import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig
 import io.confluent.kafka.serializers.KafkaAvroSerializerConfig
 import org.apache.avro.generic.GenericRecord
+import org.apache.avro.generic.IndexedRecord
 import org.apache.kafka.clients.consumer.ConsumerConfig
-import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
@@ -16,7 +14,6 @@ import org.springframework.kafka.annotation.EnableKafka
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
 import org.springframework.kafka.core.ConsumerFactory
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory
-import java.nio.charset.Charset
 
 @Configuration
 @EnableKafka
@@ -31,41 +28,14 @@ class MultipleSchemasConsumerConfiguration {
     @Value("\${schema.registry.url}")
     private lateinit var schemaRegistryUrl: String
 
-    companion object {
-        const val CUSTOMER_CREATED_EVENT_TYPE = "CustomerCreated"
-        const val CUSTOMER_ADDRESS_ADDED_EVENT_TYPE = "CustomerAddressAdded"
-        const val EVENT_TYPE_HEADER = "type"
-    }
-
     @Bean
-    fun  customerCreatedConsumerFactory(): ConsumerFactory<String, CustomerCreated> {
-        return buildConsumerFactory<CustomerCreated>(group = "group1")
-    }
-
-    @Bean
-    fun  customerAddressAddedConsumerFactory(): ConsumerFactory<String, CustomerAddressAdded> {
-        return buildConsumerFactory<CustomerAddressAdded>(group = "group2")
+    fun  specificRecordConsumerFactory(): ConsumerFactory<String, IndexedRecord> {
+        return buildConsumerFactory<IndexedRecord>(group = "group1")
     }
 
     @Bean
     fun  genericConsumerFactory(): ConsumerFactory<String, GenericRecord> {
-        return buildConsumerFactory<GenericRecord>(group = "group3", isSpecificRecord = false)
-    }
-
-    @Bean
-    fun customerCreatedKafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, CustomerCreated> {
-        val factory = ConcurrentKafkaListenerContainerFactory<String, CustomerCreated>()
-        factory.consumerFactory = customerCreatedConsumerFactory()
-        factory.setRecordFilterStrategy(::filterCustomerCreated)
-        return factory
-    }
-
-    @Bean
-    fun customerAddressAddedKafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, CustomerAddressAdded> {
-        val factory = ConcurrentKafkaListenerContainerFactory<String, CustomerAddressAdded>()
-        factory.consumerFactory = customerAddressAddedConsumerFactory()
-        factory.setRecordFilterStrategy(::filterCustomerAddressAdded)
-        return factory
+        return buildConsumerFactory<GenericRecord>(group = "group2", isSpecificRecord = false)
     }
 
     @Bean
@@ -75,16 +45,11 @@ class MultipleSchemasConsumerConfiguration {
         return factory
     }
 
-    private fun <T> filterCustomerCreated(consumerRecord: ConsumerRecord<String, T>): Boolean {
-        return getEventType(consumerRecord) != CUSTOMER_CREATED_EVENT_TYPE
-    }
-
-    private fun <T> filterCustomerAddressAdded(consumerRecord: ConsumerRecord<String, T>): Boolean {
-        return getEventType(consumerRecord) != CUSTOMER_ADDRESS_ADDED_EVENT_TYPE
-    }
-
-    private fun getEventType(record: ConsumerRecord<String, *>): String {
-        return record.headers().headers(EVENT_TYPE_HEADER).first().value().toString(Charset.defaultCharset())
+    @Bean
+    fun specificRecordKafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, IndexedRecord> {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, IndexedRecord>()
+        factory.consumerFactory = specificRecordConsumerFactory()
+        return factory
     }
 
     private fun <T> buildConsumerFactory(group: String = groupId, isSpecificRecord: Boolean = true): DefaultKafkaConsumerFactory<String, T> {
